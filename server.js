7e36d0335e04b1b400a0ecb928d874382a8d4d17
Ian McCunn <ianmccunn@gmail.com>
2015-08-13 21:31:54 -0700

    2 servers

--- server.js
+++ server.js
@@ -21,10 +21,32 @@ function connectionHandler(connection) {
     console.log('User disconnected.');
   });
 }
+
+function connectionHandler2(connection) {
+  console.log(println('A user connected.'));
+  connection.write(println('You\'ve connected.'));
+  
+  setTimeout(function() {
+    connection.write(println('the timer timed a thing'));
+  }, 4000);
+  
+  connection.on('close', function() {
+    console.log('User disconnected.');
+  });
+}
 var server = net.createServer(connectionHandler);
+var server2 = net.createServer(connectionHandler2);
+
 
 server.listen(3333, function() {
   console.log('listening');
   console.log('Server CPU Info: Cores:', nCPU);
   console.log(require('os').cpus());
 });
+
+server2.listen(8080, function() {
+  console.log('listening');
+  console.log('Server CPU Info: Cores:', nCPU);
+  console.log(require('os').cpus());
+});
+
