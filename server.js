95ddd1df9755ab98965ffe30f6da73b715556d8e
Ian McCunn <ianmccunn@gmail.com>
2016-02-27 22:48:17 -0800

    abstract server create process, parse telnet data

--- server.js
+++ server.js
@@ -9,46 +9,42 @@ function println(text) {
   return text + '\n'; 
 }
 
-function connectionHandler(connection) {
-  console.log(println('A user connected.'));
-  connection.write(println('You\'ve connected.'));
-  
-  setTimeout(function() {
-    connection.write(println('the timer timed a thing'));
-  }, 4000);
-  
-  connection.on('close', function() {
-    console.log('User disconnected.');
-  });
+function ConnectionHandler() {
+  return function(socket) {
+    console.log('A user connected.');
+    console.log('Remote Addr: ', socket.remoteAddress);
+    socket.write(println('you\'ve connected.'));
+    socket.on('data', function(data) {
+      var parsedData = data.toString('utf-8');
+      var params = parsedData.split(' ');
+      if (params[0] === 'name') {
+        socket.name = params[1];
+      }
+      console.log('User ' + socket.name + ' sent: ', data.toString('utf-8'));
+    }) 
+    socket.on('close', function() {
+      console.log('User disconnected.');
+    });
+  }
 }
 
-function connectionHandler2(connection) {
-  console.log(println('A user connected.'));
-  connection.write(println('You\'ve connected.'));
-  
-  setTimeout(function() {
-    connection.write(println('the timer timed a thing'));
-  }, 4000);
-  
-  connection.on('close', function() {
-    console.log('User disconnected.');
-  });
+function createServer(connectionHandler) {
+  return net.createServer(connectionHandler);
 }
 
-var server = net.createServer(connectionHandler);
-var server2 = net.createServer(connectionHandler2);
-
+var server = createServer(ConnectionHandler());
+var server2 = createServer(ConnectionHandler());
 
 server.listen(3333, function() {
   console.log('listening');
   console.log('Server CPU Info: Cores:', nCPU);
-  console.log(require('os').cpus());
+  // console.log(require('os').cpus());
 });
 
 server2.listen(8080, function() {
   console.log('listening');
   console.log('Server CPU Info: Cores:', nCPU);
-  console.log(require('os').cpus());
+  //console.log(require('os').cpus());
 });
 
 
